53cd7e68a8cd23d4505afdb4e2f857a7f9b2f3c4
Jin Li <jin.c.li@ericsson.com>
2020-08-03 14:14:35 +0800

    init project

---
+++ src/test/java/com/ljj/jenkins/demo/JenkinsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ljj.jenkins.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
